22916dfb75b0fba788c0945bb4a66c0e41a4e6eb
Artyom <defartyom@gmail.com>
2025-08-07 16:38:08 +0400

    Bug Fixes

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ document.write(`
     <header class="dark-header">
         <div class="container">
             <div class="logo" onclick="window.open('index.html', '_self')" style="cursor: pointer;">
-                <img src="media/icon_big.png" width="55px" style="position: relative; top: 8px;">
+                <img src="media/icon_big.png" width="50px" style="position: relative; top: 8px;">
                 <h1>BOXK Modding</h1>
                 <p>Խաղերի բողկական մոդեր՝ ԲՈՂԿ-ի Community-ից</p>
             </div>
