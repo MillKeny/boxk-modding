2f6dd5f7348ef07caea4225048618eb8259ce9f3
Artyom <defartyom@gmail.com>
2025-10-22 20:34:58 +0400

    Update header.js

--- header.js
+++ header.js
@@ -4,8 +4,9 @@ document.write(`
             <div class="logo" onclick="window.open('index.html', '_self')" style="cursor: pointer;">
                 <img src="media/icon_big.png" width="50px" style="position: relative; top: 8px;">
                 <h1>BOXK Modding</h1>
-                <p>Խաղերի բողկական մոդեր՝ ԲՈՂԿ-ի Community-ից</p>
+                <p>Խաղերի հայկական մոդեր․ ԲՈՂԿ-ի թմի սարքած կամ գտած</p>
             </div>
         </div>
     </header>
+
 `)
